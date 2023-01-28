34dfd1cfde49e0611be41ab811327db661ce4b11
Mystic <gurshabad90@gmail.com>
2023-01-29 04:05:42 +0530

    Used States and onChange-func

--- src/Components/TextForms.jsx
+++ src/Components/TextForms.jsx
@@ -1,18 +1,36 @@
 import React from "react";
+import { useState } from "react";
 
 export default function TextForms(props) {
+    
+
+    const handelClick = ()=>{
+        console.log("Clicked "+ text.toUpperCase())
+        setText(text.toUpperCase())
+    }
+    const handelOnChange = (e)=>{
+        console.log(e);
+        setText(e.target.value)
+    }
+
+
+    const [text,setText] = useState("Enetr text here(Default value)");
+    // setText("New TExt");
     return (
         <>
             <div className="mb-3  container" >
-                <label htmlFor="exampleFormControlTextarea1" className="form-label">
-                    {props.textTitle}
-                </label>
+                <h1>
+                {props.textTitle} 
+                </h1>
+                
                 <textarea
                     className="form-control"
                     id="exampleFormControlTextarea1"
                     rows="3"
+                    value={text}
+                    onChange={handelOnChange}
                 ></textarea>
-                <button className="btn btn-primary my-3">Convert to UPPERCASE</button>
+                <button className="btn btn-primary my-3" onClick={handelClick}>Convert to UPPERCASE</button>
             </div>
         </>
     );
